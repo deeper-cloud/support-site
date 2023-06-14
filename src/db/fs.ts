635e0e9beafe57e5e86aa3ed5098c2a846d8d174
asafshitrit <asaf@deeper-cloud.com>
2023-06-14 21:47:36 +0300

    using cwd now

--- src/db/fs.ts
+++ src/db/fs.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
+import path from "path";
 
-export const ValidTypes = ["questions", "topics"];
-
-//TODO: actually use FS for this, hack in the meantime
+export const ValidTypes = fs
+  .readdirSync(path.join(process.cwd(), "src", "generated"), "utf-8")
+  .map((file) => file.replace(".json", ""));
